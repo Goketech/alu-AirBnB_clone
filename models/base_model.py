7141c3d9ab060fc268f9b93236344071d0cdeb73
Modupe Akanni <modupe775@gmail.com>
2026-01-07 06:20:25 +0100

    revert changes

--- models/base_model.py
+++ models/base_model.py
@@ -6,7 +6,6 @@ This module defines the BaseModel class which serves as the base class
 for all models in the AirBnB clone project.
 """
 import uuid
-import models
 from datetime import datetime
 
 
@@ -29,6 +28,7 @@ class BaseModel:
             **kwargs: Arbitrary keyword arguments for recreating instance
                      from dictionary representation
         """
+        from models import storage
 
         if kwargs:
             # Recreate instance from dictionary
@@ -44,7 +44,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            models.storage.new(self)
+            storage.new(self)
 
     def __str__(self):
         """
@@ -64,9 +64,10 @@ class BaseModel:
         Update the updated_at attribute with current datetime and save
         to storage.
         """
+        from models import storage
 
         self.updated_at = datetime.now()
-        models.storage.save()
+        storage.save()
 
     def to_dict(self):
         """
@@ -78,10 +79,7 @@ class BaseModel:
                   converted to ISO format strings.
         """
         obj_dict = self.__dict__.copy()
-        obj_dict["__class__"] = self.__class__.__name__
-        for key, value in self.__dict__.items():
-            if key == 'created_at' or key == 'updated_at':
-                value = value.isoformat()
-            obj_dict[key] = value
-
+        obj_dict['__class__'] = self.__class__.__name__
+        obj_dict['created_at'] = self.created_at.isoformat()
+        obj_dict['updated_at'] = self.updated_at.isoformat()
         return obj_dict
